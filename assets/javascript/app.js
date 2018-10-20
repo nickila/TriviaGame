da6806c2d948b5b750eb60fd515ccfbab728862c
Brian Nickila <brian.nickila@gmail.com>
2018-10-19 21:17:21 -0500

    Reinstituded the timer. Seems to be working and reloading and not running double speed.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -51,14 +51,15 @@ $(document).ready(function () {
     // Grab a random index from the qa arrays but not index 0.
     //console.log(answerChosen);
     startGame();
-    
+
     function startGame() {
-       
+
         //$(".answer").hide();
-        $(".startButton").unbind().click(function() {
+        $(".startButton").unbind().click(function () {
             //Stuff
-       
-        //$(".startButton").on("click", function () {
+            $(".startButton").slideToggle(300);
+
+            //$(".startButton").on("click", function () {
             //$(".answer").off();
             //$(".startButton").slideUp(200);
             askQuestions();
@@ -78,33 +79,33 @@ $(document).ready(function () {
         if (answerChosen == 4) {
             display(qa4);
         }
-        
-            
-            
-           
-        
+
+
+
+
+
     }
-// function reset() {
-        // $("#question").html("");
-        // $("#timer").html("");
-        // answerChosen = 1;
-        // $("#correct").html("");
-        // $("#incorrect").html("");
-        // $("#timed-out").html("");
-        // $(".startButton").slideDown(200);
-        // // $("#timer").off();
-        // correctTotal=0;
-        // incorrectTotal=0;
-        // timedOutTotal=0;
-//         startGame();
-// }
+    // function reset() {
+    // $("#question").html("");
+    // $("#timer").html("");
+    // answerChosen = 1;
+    // $("#correct").html("");
+    // $("#incorrect").html("");
+    // $("#timed-out").html("");
+    // $(".startButton").slideDown(200);
+    // // $("#timer").off();
+    // correctTotal=0;
+    // incorrectTotal=0;
+    // timedOutTotal=0;
+    //         startGame();
+    // }
     //display(qa2);
-    
+
     function display(obj) {
         // $("#timer").off();
         $(".answer").off();
         console.log("first" + obj.answer);
-        //runTimer();
+        runTimer();
         //answerChosen=1;
 
         $("#question").html(obj.question);
@@ -117,82 +118,56 @@ $(document).ready(function () {
         $("#answer3").append(stringedAns[2]);
         $("#answer4").append(stringedAns[3]);
         //$(".answer").slideToggle(300);
-            console.log("all: " + stringedAns)
-        $(".answer").unbind().click(function() {
+        console.log("all: " + stringedAns)
+        $(".answer").unbind().click(function () {
             // if (timer > 0) {
             //     console.log("clicked: ", $(this).text())
             //     console.log("answer: ", obj.answer)
-                if ($(this).text() == (obj.answer)) {
-                    correctTotal++
-                    
-                    //stopTimer();
-                    $("#timer").html("CORRECT!");
-                    $("#timer").css({ color: "green" })
-                    //$(".answer").slideToggle(300);
-                    $("#answer1").html("");
-                    $("#answer2").html("");
-                    $("#answer3").html("");
-                    $("#answer4").html("");
-                    //setTimeout(function () {
-                    //    $("#timer").html("");
-                        //$(".answer").slideToggle(300);
-                        // $("#question").slideToggle(300);
-                        answerChosen++;
-                    //}, 2000);
-                    //setTimeout(function () {
-                        if (answerChosen == 5) {
-                            //delete obj.answer;
-                            //$(".answer").off();
-                            //$(".answer").slideToggle(300);
-                            // $("#correct").html("CORRECT: " + correctTotal);
-                            // $("#incorrect").html("INCORRECT: " + incorrectTotal);
-                            // $("#timed-out").html("TIMED-OUT: " + timedOutTotal);
-                            //$(".startButton").slideToggle(300);
-                            //$("#question").hide();
-                            answerChosen = 1;
-                            //$("#timer").off();
-                            startGame();
-                        } else {
-                            
-                        askQuestions();
-                   // }, 3000);
-                        }
+            if ($(this).text() == (obj.answer)) {
+                correctTotal++
+
+                stopTimer();
+                $("#timer").html("CORRECT!");
+                //$("#timer").css({ color: "green" })
+                //$(".answer").slideToggle(300);
+                $("#answer1").html("");
+                $("#answer2").html("");
+                $("#answer3").html("");
+                $("#answer4").html("");
+                //setTimeout(function () {
+                //    $("#timer").html("");
+                //$(".answer").slideToggle(300);
+                // $("#question").slideToggle(300);
+                answerChosen++;
+                //}, 2000);
+                //setTimeout(function () {
+
+            } else {
+                incorrectTotal++
+                console.log("else: " + obj.answer);
+                stopTimer();
+                $("#timer").html("INCORRECT!");
+                $("#answer1").html("");
+                $("#answer2").html("");
+                $("#answer3").html("");
+                $("#answer4").html("");
+                //$("#timer").html("");
+                answerChosen++;
+            }
+            if (answerChosen == 5) {
+                answerChosen = 1;
+                $(".startButton").slideToggle(300);
+                startGame();
+            } else {
+                askQuestions();
+            }
 
 
-                    // answerChosen = true;
-                    
-                } else {
-                    incorrectTotal++
-                    //delete obj.answer;
-                    console.log("else: " + obj.answer);
-                    //stopTimer();
-                    $("#timer").html("INCORRECT!");
-                    //$("#timer").css({ color: "green" })
-                    //$(".answer").slideToggle(300);
-                    $("#answer1").html("");
-                    $("#answer2").html("");
-                    $("#answer3").html("");
-                    $("#answer4").html("");
-                    //setTimeout(function () {
-                       // $("#timer").html("");
-                        //$(".answer").slideToggle(300);
-                        // $("#question").slideToggle(300);
-                        answerChosen++;
-                        if (answerChosen == 5) {
-                            answerChosen = 1;
-                            startGame();
-                        } else {
-                    //}, 2000);
-                    //setTimeout(function () {
-                        askQuestions();
-                        }
-                    //}, 3000);
 
-                  
 
-                }
+        
 
-            
+
 
         });
 
@@ -202,44 +177,49 @@ $(document).ready(function () {
 
 
 
-    
-
-    // var timer = 16;
 
 
-    // function runTimer() {
-    //     timer = 16;
-    //     intervalId = setInterval(decrement, 1000);
-    // }
-    // function decrement() {
-    //     if (timer > 0) {
-    //         timer--;
-    //         if (timer < 10) {
-    //             $("#timer").html("0" + timer + " SECONDS")
-    //         } else {
-    //             $("#timer").html(timer + " SECONDS");
-    //         }
-    //     } else {
-    //         stopTimer();
-    //         timedOutTotal++
-    //         answerChosen++
-    //         $("#timer").html("TIME'S UP!");
-    //         //$(".answer").slideToggle(300);
-    //         $("#answer1").html("");
-    //         $("#answer2").html("");
-    //         $("#answer3").html("");
-    //         $("#answer4").html("");
-    //         setTimeout(function () {
-    //             askQuestions();
-    //         }, 3000);
+var timer = 16;
 
-    //     }
-    // }
-    // function stopTimer() {
-    //     clearInterval(intervalId);
-    // }
 
-    
+function runTimer() {
+    timer = 16;
+    intervalId = setInterval(decrement, 1000);
+}
+function decrement() {
+    if (timer > 0) {
+        timer--;
+        if (timer < 10) {
+            $("#timer").html("0" + timer + " SECONDS")
+        } else {
+            $("#timer").html(timer + " SECONDS");
+        }
+    } else {
+        stopTimer();
+        timedOutTotal++
+        answerChosen++
+        $("#timer").html("TIME'S UP!");
+        $("#answer1").html("");
+        $("#answer2").html("");
+        $("#answer3").html("");
+        $("#answer4").html("");
+        //setTimeout(function () {
+            if (answerChosen == 5) {
+                answerChosen = 1;
+                startGame();
+            } else {
+                askQuestions();
+            }
+            
+        //}, 3000);
+
+    }
+}
+function stopTimer() {
+    clearInterval(intervalId);
+}
+
+
 });
 
 
